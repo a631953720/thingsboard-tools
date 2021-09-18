abf209639aa9eb63153c344d0f7b8e8bc680e97b
kk <tairong_kang@innodisk.com>
2021-09-18 19:58:31 +0800

    feat: Save device error log.

--- helpers/saveOutput.js
+++ helpers/saveOutput.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 const { FILE, DEVICE } = require('../constant/env');
 const showLog = require('../helpers/showMsgOnLog');
+const deviceList = require(`../output/${DEVICE.deviceListFileName}`);
 
-function Counter(count) {
+function saveTestInformation(count) {
     showLog('Save output...');
 
     const jsonPath = FILE.LogFilePath;
-    const deviceList = DEVICE.deviceList;
     const data = JSON.stringify({
-        deviceNameList: deviceList.map(device=>device.name),
-        testCount: count
+        deviceCount: deviceList.length,
+        testCount: count,
+        updateTime: new Date().toLocaleString()
     });
 
     fs.writeFileSync(jsonPath, data, (err) => {
@@ -17,4 +18,21 @@ function Counter(count) {
     });
 }
 
-module.exports = Counter;
+function saveErrorDeviceList(errorDeviceList) {
+    showLog('Save error device list...');
+
+    const jsonPath = FILE.ErrorLogFilePath;
+    const data = JSON.stringify({
+        ...errorDeviceList,
+        updateTime: new Date().toLocaleString()
+    });
+
+    fs.writeFileSync(jsonPath, data, (err) => {
+        console.error('Data written to file error', err);
+    });
+}
+
+module.exports = {
+    saveTestInformation,
+    saveErrorDeviceList
+};
