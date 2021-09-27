a3540c7f2528f155c3be1f25175c91974ffd5157
kk <tairong_kang@innodisk.com>
2021-09-27 23:19:15 +0800

    fix: deleteDevice show log error

--- application/deleteDevice.js
+++ application/deleteDevice.js
@@ -2,9 +2,15 @@ const { showLog } = require('../helpers/showMsgOnLog');
 const deleteAllDevice = require('../controllers/devices/deleteAllDevice');
 
 async function deleteTBDevice() {
-    showLog('delete all devices...');
-    await deleteAllDevice();
-    showLog('done');
+    showLog('Delete all devices...');
+
+    const response = await deleteAllDevice();
+
+    if (response.status >= 400) {
+        showLog('Delete all devices error');
+    } else {
+        showLog('Done');
+    }
 }
 
 deleteTBDevice();
