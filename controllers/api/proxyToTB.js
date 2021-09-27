90a4d858852164d4ec199cfc562f0654a9341ee2
kk <tairong_kang@innodisk.com>
2021-09-27 21:36:01 +0800

    feat: add proxy to TB default error object

--- controllers/api/proxyToTB.js
+++ controllers/api/proxyToTB.js
@@ -1,11 +1,14 @@
 const APICaller = require('../../helpers/apiCaller');
 const { getTenantJWTToken } = require('../user/getTenantToken');
+const { defaultError } = require('../../constant/defaultError');
 
 let token;
 
+// add error handler
+
 async function tryGetTokenAndResendRequest(config) {
     token = await getTenantJWTToken();
-
+    // response token or error object.
     return APICaller({
         ...config,
         headers: {
@@ -25,14 +28,20 @@ async function proxyToTB(config) {
                 'X-Authorization': `Bearer ${token}`,
             },
         };
-        // 執行時間過長會出現token失效
         const response = await APICaller(newConfig);
 
+        // response token or error object.
         if (response.status === 401) return tryGetTokenAndResendRequest(newConfig);
 
         return response;
     } catch (error) {
         console.error('Proxy to TB error:', error);
+        return {
+            ...defaultError,
+            data: {
+                message: 'Proxy to TB error',
+            },
+        };
     }
 }
 
